023afaa423d9fbc77e01aaeb2d2762e1e3de1d0b
kubaamil <dudajakub95@gmail.com>
2020-03-22 15:28:42 +0100

    scalable logo component

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Header from "./components/root/Header";
+import Logo from "./components/Logo";
 import Menu from "./components/root/Menu";
 
 
 const Root = () => {
 
     return <div className="main container">
-        <Header/>
+        <div className="main__header">
+            <Logo scale={1}/>
+        </div>
         <Menu/>
         <img src="" alt="" className="main__sun"/>
     </div>
---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import pencil from "../assets/pencil.png";
+import React from "react";
+
+const Logo = ({scale}) => {
+
+    return <div className="logo" style={{transform: `scale(${scale})`}}>
+        <h1 className="logo__title main__text">PaństwaMiasta</h1>
+        <img src="" alt="Map" className="logo__map"/>
+        <img src={pencil} alt="Pencil" className="logo__pencil"/>
+    </div>
+
+};
+
+export default Logo
