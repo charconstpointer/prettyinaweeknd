5d6d574464013dfd3418d1586b6b2dc3f00c8793
kubaamil <dudajakub95@gmail.com>
2020-03-28 15:02:51 +0100

    lobby view

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import human from '../assets/human.png'
+
+const users = [
+    'kuba', 'pati', 'ktos', 'zdzisław'
+]
 
 const Lobby = () => {
 
-    return <h1>Lobby</h1>
+    return <div className={"lobby"}>
+        <div className={"lobby__room"}>
+            <div className={'wrapper'}>
+                <img src={human} alt="człowieczek"/>
+                <h3 className={"main__text"}>Lista graczy i punkty</h3>
+            </div>
+            <div className={'lobby__people'}>
+                {users.map((e, i) => <span className={"main__text"}>{i+1}. {e}</span>)}
+            </div>
+        </div>
+        <div className={"lobby__chat"}>
+            <div className={"lobby__messages"}>
+                <p><span className={"bold"}>KAZIK: </span><span>TESKTASDdikjswabijdbiwsabdWASD jakis tam tekst wsobie wiadomosci a ciekawe co to</span></p>
+            </div>
+            <input type="text" placeholder={"Napisz wiadomość..."}/>
+        </div>
+    </div>
 };
 
 export default Lobby
